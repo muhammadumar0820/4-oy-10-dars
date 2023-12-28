a6d19aaa9638169462e869a15d99e99e7e97bdf4
muhammadumar0820 <salimjonovmuhammadumar372@gmail.com>
2023-12-28 19:53:43 +0300

    masalalar

---
+++ index.js
@@ -0,0 +1,19 @@
+//Birinchi masala
+//function rever(arg){
+//    let arr = ['hello'];
+//    arr.reverse();
+//}
+//rever();
+
+//Ikkinchi masala
+
+//Uchinchi masala
+//function pallind(arg){
+//    let arr = ['radar'];
+//    if(arr == arr.reverse){
+//        console.log('pallindrom');
+//    }else{
+//        console.log('pallindrom emas');
+//    }
+//    return;
+//}
